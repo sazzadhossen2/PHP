d11eaa11a339580abfb3966e0c4d3d4900a067f5
sazzadhossen2 <89637675+sazzadhossen2@users.noreply.github.com>
2024-07-11 00:53:20 +0600

    first commit

---
+++ week 4/test.php
@@ -0,0 +1,46 @@
+<?php 
+// Simple print
+// $name="Shamim";
+// $id ="221-15-5144";
+// // echo $id;
+// echo $id;
+
+
+// constant define
+// define("LANGUAGE","Bangla");
+// echo LANGUAGE;
+
+
+// Local Function
+// function sum(){
+//     $num1=100;
+//     $num2=200;
+//     $res=$num1 +$num2;
+//     echo $res;
+// }
+// sum()
+
+
+
+// Global Function
+// $num1=500;
+// $num2=100;
+// function sum(){
+//     global $num1,$num2;
+// $res=$num1+$num2;
+// echo $res;
+// }
+// sum();
+
+
+
+// $num1=100;
+// $num2=200;
+// $num3=300;
+// $num4=400;
+// $num5=500;
+// $num6=600;
+// $sum=$num1+$num2+$num3+$num4+$num5+$num6;
+// echo $sum;
+
+?>
