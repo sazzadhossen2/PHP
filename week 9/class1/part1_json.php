aed10debcb447c21c7fe65ae46c03244319d295d
sazzadhossen2 <89637675+sazzadhossen2@users.noreply.github.com>
2024-08-30 02:02:03 +0600

    done

---
+++ week 9/class1/part1_json.php
@@ -0,0 +1,31 @@
+<?php 
+
+
+//PHP Encoded
+// single Dimention Array to JSON object
+$AsocArray=["name"=>"Sazzad","age"=>22];
+$JSON=json_encode($AsocArray);
+echo $JSON;
+
+
+
+//Multi Dimention Arraay to Json Array
+$AsocArray=[
+    ["name"=>"Sazzad","age"=>22],
+    ["name"=>"Shamim","age"=>25]
+];
+$JSON=json_encode($AsocArray);
+echo $JSON;
+
+
+//PHP Decode
+// json object to single Dimention Array
+$StringData ='{"name":"Sazzad","age":23}';
+$PHPAsocArray = json_decode($StringData, true);
+print_r($PHPAsocArray);
+
+
+//json Array to Multi Dimention Array
+$StringData ='[{"name":"Sazzad","age":23},{"name":"Shamim","age":24}]';
+$PHPAsocArray = json_decode($StringData, true);
+print_r($PHPAsocArray);
